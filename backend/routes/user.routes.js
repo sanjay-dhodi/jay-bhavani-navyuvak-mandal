8183a74ed0a8318ad4742e7272c59992186cf760
sanjay dhodi <sdhodi157@gmail.com>
2025-12-08 20:14:15 +0530

    git pull issue

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const router = express.Router();
-const {
-  fetchAllUser,
-  createUser,
-  deleteUser,
-  updateUser,
-} = require("../controllers/user.controllers");
-const { verifyJwt } = require("../middleweres/authMiddlewere");
+// const express = require("express");
+// const router = express.Router();
+// const {
+//   fetchAllUser,
+//   createUser,
+//   deleteUser,
+//   updateUser,
+// } = require("../controllers/user.controllers");
+// const { verifyJwt } = require("../middleweres/authMiddlewere");
 
 // router.get("/admin/user/getuser", verifyJwt, fetchAllUser);
 // router.post("/admin/user/create", verifyJwt, createUser);
