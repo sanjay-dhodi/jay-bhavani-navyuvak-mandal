6b03f19290c8b45393a5751d6dd9c7eb9f3fdace
sanjay dhodi <sdhodi157@gmail.com>
2025-12-17 14:24:20 +0530

    add logo

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { Link } from "react-router";
+import { Image } from "@imagekit/react";
 
 export default function Navbar() {
   return (
-    <div className="navbar  bg-yellow-300 border-b-4 border-yellow-400 flex justify-center sticky z-50 top-0 ">
-      <div>
-        <Link to="/">
-          <h1 className="btn btn-ghost text-2xl text-orange-700 py[40px] sm:py-7 sm:text-4xl">
-            Jay Bhavani Navyuvak Mandal
-          </h1>
-        </Link>
-      </div>
+    <div className="navbar h-25 sm:h-20  bg-white border-b-4 border-orange-400 flex justify-center sticky z-50 top-0 ">
+      <Image
+        urlEndpoint={import.meta.env.VITE_IMAGEKIT_ENDPOINT}
+        src="jay bhavani/jay-bhavani-logo.png"
+        alt="Jay Bhavani logo"
+        className="sm:relative sm:top-6 h-23 w-23 sm:h-30  sm:w-30 md:h-35 md:w-35 "
+      />
     </div>
   );
 }
